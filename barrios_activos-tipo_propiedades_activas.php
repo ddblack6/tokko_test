a0cd1cf50cb9ae07dabb90c029318c6cc6b119ff
Dario <dariogabrielarias@gmail.com>
2020-11-28 21:12:56 -0300

    Update barrios_activos-tipo_propiedades_activas.php

--- barrios_activos-tipo_propiedades_activas.php
+++ barrios_activos-tipo_propiedades_activas.php
@@ -1,6 +1,14 @@
 <?php
 include "api.php";
 
+
+#Para ejecutar este ejemplo por URL
+/*
+http://localhost/barrios_activos-tipo_propiedades_activas.php?order_by=price&limit=200&order=desc&page=1&data={%22current_localization_id%22:0,%22current_localization_type%22:%22country%22,%22price_from%22:0,%22price_to%22:999999999,%22operation_types%22:[1,2,3],%22property_types%22:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],%22currency%22:%22ANY%22,%22filters%22:[]}
+*/
+
+
+
 //CHANGE THIS APIKEY WITH YOUR REAL STATE APIKEY
 // $auth = new TokkoAuth('5940ea45eb7cfb55228bec0b958ea9c0be151757');  //demo key
 
